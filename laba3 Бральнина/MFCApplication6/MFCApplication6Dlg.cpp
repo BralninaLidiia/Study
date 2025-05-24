339c0eac57a53fd44a5d27a74eaef30ee58bc423
BralninaLidiia <144156506+BralninaLidiia@users.noreply.github.com>
2025-05-24 19:56:00 +0300

    Add files via upload

---
+++ laba3 Бральнина/MFCApplication6/MFCApplication6Dlg.cpp
@@ -0,0 +1,227 @@
+﻿
+// MFCApplication6Dlg.cpp: файл реализации
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication6.h"
+#include "MFCApplication6Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Диалоговое окно CAboutDlg используется для описания сведений о приложении
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+// Реализация
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Диалоговое окно CMFCApplication6Dlg
+
+
+
+CMFCApplication6Dlg::CMFCApplication6Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION6_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication6Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, spisok);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication6Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_NOTIFY(LVN_ITEMCHANGED, IDC_LIST1, &CMFCApplication6Dlg::OnItemchangedList1)
+END_MESSAGE_MAP()
+
+
+// Обработчики сообщений CMFCApplication6Dlg
+
+BOOL CMFCApplication6Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Добавление пункта "О программе..." в системное меню.
+
+	// IDM_ABOUTBOX должен быть в пределах системной команды.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Задает значок для этого диалогового окна.  Среда делает это автоматически,
+	//  если главное окно приложения не является диалоговым
+	SetIcon(m_hIcon, TRUE);			// Крупный значок
+	SetIcon(m_hIcon, FALSE);		// Мелкий значок
+
+	spisok.InsertColumn(0, L"Метод подсчета", LVCFMT_LEFT, 200);
+	spisok.InsertItem(0, L"Золотое сечение", -1);
+	spisok.InsertItem(1, L"Равномерный поиск", -1);
+	spisok.InsertItem(2, L"Половинное деление", -1);
+	spisok.InsertItem(3, L"Фибоначчи", -1);
+	
+
+	return TRUE;  // возврат значения TRUE, если фокус не передан элементу управления
+}
+
+void CMFCApplication6Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// При добавлении кнопки свертывания в диалоговое окно нужно воспользоваться приведенным ниже кодом,
+//  чтобы нарисовать значок.  Для приложений MFC, использующих модель документов или представлений,
+//  это автоматически выполняется рабочей областью.
+
+void CMFCApplication6Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // контекст устройства для рисования
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Выравнивание значка по центру клиентского прямоугольника
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Нарисуйте значок
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// Система вызывает эту функцию для получения отображения курсора при перемещении
+//  свернутого окна.
+HCURSOR CMFCApplication6Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+double CMFCApplication6Dlg::Func(double x)
+{
+	return tan(x) * log(x) / exp(x);
+}
+
+CString CMFCApplication6Dlg::Zolotoe_sechenie(double a, double b)
+{
+	CString str;
+	double x1, x2;
+	while (true)
+	{
+		x1 = b - (b - a) / F;
+		x2 = a + (b - a) / F;
+		if (Func(x1) > Func(x2))
+		{
+			a = x1;
+		}	
+		else
+		{
+			b = x2;
+		}
+			
+		if (abs(b - a) < e)
+		{
+			str.Format(L"%f\r\n", (a + b) / 2.0);
+			return str;
+		}
+	}
+}
+
+void CMFCApplication6Dlg::OnItemchangedList1(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMLISTVIEW pNMLV = reinterpret_cast<LPNMLISTVIEW>(pNMHDR);
+	POSITION pos = spisok.GetFirstSelectedItemPosition();
+	while (pos != NULL)
+	{
+		int index = spisok.GetNextSelectedItem(pos);
+		if (index == 0)
+		{
+			double a1 = a;
+			while (true)
+			{
+				b = a + 0.1;
+				if (Func(a) * Func(b) < 0)
+					break;
+				a += 0.01;
+			}
+			a = a1;
+			MessageBox(L"Метод золотого сечения "+Zolotoe_sechenie(a,b));
+		}
+		else if (index == 1)
+		{
+			MessageBox(L"Метод равномерного поиска");
+		}
+		else if (index == 2)
+		{
+			MessageBox(L"Метод половинного деления");
+		}
+		else
+		{
+			MessageBox(L"Метод Фибоначчи");
+		}
+	}	
+	*pResult = 0;
+}
+
