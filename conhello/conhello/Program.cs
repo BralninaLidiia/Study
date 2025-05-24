bbe674b6b65b686e9c32a305c1424723aee17f1c
BralninaLidiia <144156506+BralninaLidiia@users.noreply.github.com>
2025-05-24 20:48:53 +0300

    Add files via upload

---
+++ conhello/conhello/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+///<summary>
+///Вернуть прямоугольник в виде массива строк
+///</summary>
+namespace cnsPrintRectangle
+{
+    internal class Program
+    {
+        /// <summary> 
+        /// лол 
+        /// </summary> 
+
+        static string[] GetRectangle(int width, int height, char symbol = '*', bool isFill = true, char clear = ' ')
+        {
+
+            List<string> result = new();
+            for (int i = 0; i < height; i++)
+                result.Add((isFill || i == 0 || i == height - 1) ? (new string(symbol, width)) : (symbol + new string(clear, width - 2) + symbol));
+
+            return result.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ширина высота символ рисования заполнить фигуру?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            int height = Convert.ToInt32(Console.ReadLine());
+            char symbol = Convert.ToChar(Console.ReadLine());
+            bool isFill = Console.ReadLine().ToLower() == "y";
+
+            var shape = GetRectangle(10, 5);
+
+            foreach (var line in shape)
+                Console.WriteLine(line);
+
+        }
+    }
+}
