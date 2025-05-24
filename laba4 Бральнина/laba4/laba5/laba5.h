81e15438c0d08bdacb30e07f556544c36f37d938
BralninaLidiia <144156506+BralninaLidiia@users.noreply.github.com>
2025-05-24 19:57:51 +0300

    Add files via upload

---
+++ laba4 Бральнина/laba4/laba5/laba5.h
@@ -0,0 +1,32 @@
+﻿
+// laba5.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// Claba5App:
+// Сведения о реализации этого класса: laba5.cpp
+//
+
+class Claba5App : public CWinApp
+{
+public:
+	Claba5App();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Claba5App theApp;
---
+++ laba4 Бральнина/laba4/laba5/laba5Dlg.h
@@ -0,0 +1,43 @@
+﻿
+// laba5Dlg.h: файл заголовка
+//
+
+#pragma once
+
+// Диалоговое окно Claba5Dlg
+class Claba5Dlg : public CDialogEx
+{
+// Создание
+public:
+	Claba5Dlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LABA5_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+
+public:
+	double MSQ(double a, double b, double r);
+	void LinearDixotomy(double& value1, double value2, double value3, int type);
+	CString result();
+	CString Otklonenie(double x, double y);
+	afx_msg void OnBnClickedButton1();
+	CString answer;
+	double a, b;
+};
