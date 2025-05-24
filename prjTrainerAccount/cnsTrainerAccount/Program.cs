3fafd4a732f4a78d278b46e14398b426d18f9344
BralninaLidiia <144156506+BralninaLidiia@users.noreply.github.com>
2025-05-24 20:53:51 +0300

    Add files via upload

---
+++ prjTrainerAccount/cnsTrainerAccount/Program.cs
@@ -0,0 +1,18 @@
+﻿using libCore;
+
+internal class Program
+{
+    private static Game g;
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Игра устный счет");
+        Console.WriteLine();
+
+        g = new Game();
+        g.ChangeQuestion += () => Console.WriteLine($"Вопрос: {g.QuestionLine}");
+
+        var dt = DateTime.Now;
+        Console.WriteLine($"Ты играл {DateTime.Now - dt} секунд");
+    }
+}
---
+++ prjTrainerAccount/wfaTrainerAccount/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace wfaTrainerAccount
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label2 = new Label();
+            label1 = new Label();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            buYes = new Button();
+            buNo = new Button();
+            laQuestion = new Label();
+            label4 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label2, 1, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(844, 96);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(255, 128, 128);
+            label2.Dock = DockStyle.Fill;
+            label2.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(425, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(416, 96);
+            label2.TabIndex = 1;
+            label2.Text = "Неверно = 0";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(128, 255, 128);
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(416, 96);
+            label1.TabIndex = 0;
+            label1.Text = "Верно = 0";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(buNo, 1, 0);
+            tableLayoutPanel2.Controls.Add(buYes, 0, 0);
+            tableLayoutPanel2.Location = new Point(12, 425);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(841, 100);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            buYes.BackColor = Color.Snow;
+            buYes.Dock = DockStyle.Fill;
+            buYes.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            buYes.ForeColor = Color.Green;
+            buYes.Location = new Point(3, 3);
+            buYes.Name = "button1";
+            buYes.Size = new Size(414, 94);
+            buYes.TabIndex = 0;
+            buYes.Text = "Да";
+            buYes.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            buNo.BackColor = Color.Snow;
+            buNo.Dock = DockStyle.Fill;
+            buNo.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            buNo.ForeColor = Color.FromArgb(192, 0, 0);
+            buNo.Location = new Point(423, 3);
+            buNo.Name = "button2";
+            buNo.Size = new Size(415, 94);
+            buNo.TabIndex = 1;
+            buNo.Text = "Нет";
+            buNo.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            laQuestion.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            laQuestion.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            laQuestion.Location = new Point(15, 108);
+            laQuestion.Name = "label3";
+            laQuestion.Size = new Size(841, 266);
+            laQuestion.TabIndex = 2;
+            laQuestion.Text = "10+11=21";
+            laQuestion.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label4.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(15, 374);
+            label4.Name = "label4";
+            label4.Size = new Size(835, 48);
+            label4.TabIndex = 3;
+            label4.Text = "Верно?";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(868, 537);
+            Controls.Add(label4);
+            Controls.Add(laQuestion);
+            Controls.Add(tableLayoutPanel2);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(683, 380);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label2;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button buNo;
+        private Button buYes;
+        private Label laQuestion;
+        private Label label4;
+    }
+}
